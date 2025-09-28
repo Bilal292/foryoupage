241b4c9df2beba3bc2e991ee7fe45a84e24e12eb
Bilal Ahmad Malik <92743737+Bilal292@users.noreply.github.com>
2025-09-28 20:07:53 +0100

    added database indexes

--- myapp/models.py
+++ myapp/models.py
@@ -14,6 +14,14 @@ class Pin(models.Model):
 
     platform = models.CharField(max_length=100, blank=True, null=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['latitude']),
+            models.Index(fields=['longitude']),
+            models.Index(fields=['latitude', 'longitude']),  # Composite index for bounds queries
+            models.Index(fields=['is_active']), 
+        ]
+
     def __str__(self):
         return f"{self.link} ({self.latitude}, {self.longitude})"
     
